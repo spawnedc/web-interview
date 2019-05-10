7209780385e7ad5a906c02d0074459ce8c51f971
Can Yilmaz <spawnedc@gmail.com>
2019-05-10 23:32:04 +0100

    Add form validation, more styling

--- src/components/BookAppointment/BookAppointment.js
+++ src/components/BookAppointment/BookAppointment.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { FaRegClock, FaStethoscope, FaVideo } from 'react-icons/fa'
+import {
+  FaNotesMedical,
+  FaRegClock,
+  FaRegImages,
+  FaStethoscope,
+  FaVideo,
+} from 'react-icons/fa'
+import UserBox from '../../containers/UserBox'
 import { getAvailableSlots } from '../../services/api'
 import { Pills } from '../Pills/Pills'
-import User from '../User'
 import './BookAppointment.scss'
 
 class BookAppointment extends React.Component {
@@ -36,6 +42,16 @@ class BookAppointment extends React.Component {
     this.props.setAppointmentType(appointmentType)
   }
 
+  canSubmitForm = () => {
+    const { appointment } = this.props
+
+    return (
+      appointment.consultantType &&
+      appointment.selectedSlot &&
+      appointment.appointmentType
+    )
+  }
+
   componentDidMount() {
     getAvailableSlots()
       .then(slots => this.props.setAppointmentSlots(slots))
@@ -51,90 +67,114 @@ class BookAppointment extends React.Component {
       <section className="book-appointment">
         <h1 className="section-title">New Appointment</h1>
 
-        <User />
+        <UserBox userId={1} />
 
         <form className="appointmen-form">
-          <fieldset className="form-section">
-            <legend className="form-section-title">
-              <span className="form-section-icon">
-                <FaStethoscope />
-              </span>
-              <span className="form-section-title-label">Consultant Type</span>
-            </legend>
-            <div className="form-section-content">
-              <Pills
-                name="consultant-type"
-                items={consultantTypes}
-                onItemClick={this.onConsultantTypeClick}
-                selectedItem={appointment.consultantType}
-              />
-
-              {appointment.consultantType && (
-                <p>{`Babylon ${appointment.consultantType}`}</p>
-              )}
-            </div>
-          </fieldset>
-
-          {appointmentSlots.available.length > 0 && (
+          <div className="form-sections-wrapper">
             <fieldset className="form-section">
               <legend className="form-section-title">
                 <span className="form-section-icon">
-                  <FaRegClock />
+                  <FaStethoscope />
                 </span>
                 <span className="form-section-title-label">
-                  Date &amp; Time
+                  Consultant Type
                 </span>
               </legend>
               <div className="form-section-content">
                 <Pills
-                  name="date-time"
-                  items={appointmentSlots.available}
-                  getItemDisplayValue={slot => slot.displayValue}
-                  onItemClick={this.onAppointmentSlotClick}
-                  selectedItem={appointment.selectedSlot}
+                  name="consultant-type"
+                  items={consultantTypes}
+                  onItemClick={this.onConsultantTypeClick}
+                  selectedItem={appointment.consultantType}
                 />
+
+                {appointment.consultantType && (
+                  <small className="selected-consultant-type">{`Babylon ${
+                    appointment.consultantType
+                  }`}</small>
+                )}
               </div>
             </fieldset>
-          )}
 
-          {appointment.selectedSlot && (
+            {appointmentSlots.available.length > 0 && (
+              <fieldset className="form-section">
+                <legend className="form-section-title">
+                  <span className="form-section-icon">
+                    <FaRegClock />
+                  </span>
+                  <span className="form-section-title-label">
+                    Date &amp; Time
+                  </span>
+                </legend>
+                <div className="form-section-content">
+                  <Pills
+                    name="date-time"
+                    items={appointmentSlots.available}
+                    getItemDisplayValue={slot => slot.displayValue}
+                    onItemClick={this.onAppointmentSlotClick}
+                    selectedItem={appointment.selectedSlot}
+                  />
+                </div>
+              </fieldset>
+            )}
+
+            {appointment.selectedSlot && (
+              <fieldset className="form-section">
+                <legend className="form-section-title">
+                  <span className="form-section-icon">
+                    <FaVideo />
+                  </span>
+                  <span className="form-section-title-label">
+                    Appointment Type
+                  </span>
+                </legend>
+                <div className="form-section-content">
+                  <Pills
+                    name="appointment-type"
+                    items={appointment.selectedSlot.appointmentType}
+                    onItemClick={this.onAppointmentTypeClick}
+                    selectedItem={appointment.appointmentType}
+                  />
+                </div>
+              </fieldset>
+            )}
+
             <fieldset className="form-section">
               <legend className="form-section-title">
                 <span className="form-section-icon">
-                  <FaVideo />
+                  <FaNotesMedical />
                 </span>
-                <span className="form-section-title-label">
-                  Appointment Type
-                </span>
+                <span className="form-section-title-label">Notes</span>
               </legend>
               <div className="form-section-content">
-                <Pills
-                  name="appointment-type"
-                  items={appointment.selectedSlot.appointmentType}
-                  onItemClick={this.onAppointmentTypeClick}
-                  selectedItem={appointment.appointmentType}
+                <textarea
+                  className="appointment-notes"
+                  placeholder="Describe your symptoms"
                 />
               </div>
             </fieldset>
-          )}
+
+            <fieldset className="form-section">
+              <legend className="form-section-title">
+                <span className="form-section-icon">
+                  <FaRegImages />
+                </span>
+                <span className="form-section-title-label">Attach a photo</span>
+              </legend>
+              <div className="form-section-content">
+                <input type="file" />
+              </div>
+            </fieldset>
+          </div>
+
+          <button
+            disabled={!this.canSubmitForm()}
+            className="submit-button"
+            type="submit"
+          >
+            Book
+          </button>
         </form>
-
-        <div style={{ maxWidth: 600, margin: '24px auto' }}>
-          <div>
-            <strong>Notes</strong>
-            <textarea />
-          </div>
-          <div>
-            <button
-              className="button"
-              onClick={() => {
-                /* TODO: submit the data */
-              }}
-            >
-              Book appointment
-            </button>
-          </div>
-        </div>
       </section>
     )
   }
