b66378586e89685a79126bd19603817103a409f5
Can Yilmaz <spawnedc@gmail.com>
2019-05-12 18:09:14 +0100

    Fix minutes in date formats

--- src/constants/dateTimeFormats.js
+++ src/constants/dateTimeFormats.js
@@ -1,3 +1,3 @@
-export const HOURS_MINUTES = 'HH:MM'
-export const SHORT_DATE_TIME = 'D MMM @ HH:MM'
-export const LONG_DATE_TIME = 'ddd, D MMM @ HH:MM'
+export const HOURS_MINUTES = 'HH:mm'
+export const SHORT_DATE_TIME = 'D MMM @ HH:mm'
+export const LONG_DATE_TIME = 'ddd, D MMM @ HH:mm'
